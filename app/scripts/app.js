b0e0ab04bfc4e4750073a72944ccac4ef789ebee
Rupal TestLocal <rupal@merasport.com>
2017-06-15 11:53:26 +0530

    with scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-angular.module('confusionApp', []).controller(
-				'menuController', function() {
-					this.tab = 1;
-					this.filtText = '';
-					var dishes = [
+angular.module('confusionApp', [])
+		.controller(
+				'MenuController',  ['$scope', function($scope) {
+					$scope.tab = 1;
+					$scope.filtText = '';
+					$scope.dishes = [
 							{
 								name : 'Uthapizza',
 								image : 'images/uthapizza.png',
@@ -42,26 +43,26 @@ angular.module('confusionApp', []).controller(
 								comment : ''
 							} ];
 
-					this.dishes = dishes;
+					//$scope.dishes = dishes; not reqd since we already have $scope.dishes= [...
 
-					this.select = function(setTab) {
-						this.tab = setTab;
+					$scope.select = function(setTab) {
+						$scope.tab = setTab;
 
 						if (setTab === 2) {
-							this.filtText = "appetizer";
+							$scope.filtText = "appetizer";
 						}
 						else if (setTab === 3) {
-							this.filtText = "mains";
+							$scope.filtText = "mains";
 						}
 						else if (setTab === 4) {
-							this.filtText = "dessert";
+							$scope.filtText = "dessert";
 						}
 						else {
-							this.filtText = "";
+							$scope.filtText = "";
 						}
 					};
 
-					this.isSelected = function(checkTab) {
-						return (this.tab === checkTab);
+					$scope.isSelected = function(checkTab) {
+						return ($scope.tab === checkTab);
 					};
-				});
+				}]);
